14d744d81de2a0a30346c7024bdce64a20294008
Aulie <auliemth@gmail.com>
2026-05-21 15:02:17 +0800

    ini fix banget dan save all file

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const lyrics = [
   { text: "I've been on the road since I was sixteen", time: 0.1, duration: 3.4, side: "left" },
   { text: "they don't really notice how I see things", time: 3.5, duration: 3.6, side: "right" },
   { text: "these girls they come and go between my bedsheets", time: 7, duration: 3.5, side: "left" },
-  { text: "and I've been doing blue and causing big scenes, yeah", time: 8, duration: 3.8, side: "right" },
+  { text: "and I've been doing blue and causing big scenes, yeah", time: 11, duration: 3.8, side: "right" },
   { text: "pull up and I'm higher than the big trees, yeah", time: 14.6, duration: 3.8, side: "left" },
   { text: "she don't really like it but she needs me, yeah", time: 18, duration: 4.5, side: "right" },
   { text: "she saying she don't really miss me", time: 22, duration: 3.5, side: "left" },
